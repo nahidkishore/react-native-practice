5c5a2edcb3c53352b33dc5387731ebe4c7ced5ca
nahid <nahidkishore99@gmail.com>
2021-02-09 14:29:57 +0600

    iniline styling scrollview

--- App.js
+++ App.js
@@ -1,52 +1,60 @@
-import React from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+  const goalInputHandler = (enterText) => {
+    setEnteredGoal(enterText);
+  };
+  const addGoalHandler = () => {
+    /*  setCourseGoals([...courseGoals,enteredGoal]) */
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
   return (
-    <View
-      style={{
-        padding: 50,
-        flexDirection: 'row',
-        width: '80%',
-        height: 300,
-        justifyContent: 'space-around',
-        alignItems: 'stretch',
-      }}
-    >
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'red',
-
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Text>1</Text>
+ 
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Write here...'
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title='Click Me Baby' color='green' onPress={addGoalHandler} />
       </View>
-      <View
-        style={{
-          flex: 2,
-          backgroundColor: 'green',
-
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'blue',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Text>3</Text>
-      </View>
+      <ScrollView>
+        {courseGoals.map((goal) => (
+          <View key={goal} style={styles.listItem}>
+            <Text >{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
+  
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 5,
+  },
+  listItem: {
+    padding: 10,
+    marginTop: 5,
+    borderColor: 'black',
+    backgroundColor: 'gray',
+    borderWidth: 1,
+  },
+});
